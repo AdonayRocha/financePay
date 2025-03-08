46fab083f794ac7d8127b415101de2c58e96f32f
adonayrocha <adonay.rodrigues.rocha@gmail.com>
2025-03-07 21:58:18 -0300

    basic

---
+++ financePay/src/test/java/com/financePay/financePay/FinancePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financePay.financePay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
